bdc3f433263db0a7720793e1d4ae1b3db4adde85
Sajid-Hasan2007 <sajidhasanapp@gmail.com>
2023-03-23 09:35:51 +0600

    Day-4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import "./App.css";
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Test from "./components/Test";
 
-function App() {
-  const [text, setText] = useState("sajid");
-  return (
-    <>
-      <Test name={text} details={{ phone: "04323", pass: "600" }} />
-      <button onClick={() => setText("fahim")}>Click me</button>
-    </>
-  );
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "sajid",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Test name={this.state.name} />
+        <button
+          onClick={() => {
+            this.setState({ name: "fahim" });
+          }}
+        >
+          Click me
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Test(props) {
-  return (
-    <div>
-      <h1>Hello {props.name}</h1>
-      <h1> {props.details.phone}</h1>
-      <h1> {props.details.pass}</h1>
-    </div>
-  );
+export class Test extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     data: 1,
+  //   };
+  // }
+  // clickHandler() {
+  //   this.setState({ data: this.state.data + 1 });
+  // }
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.props.name}</h1>
+      </div>
+    );
+  }
 }
 
 export default Test;
