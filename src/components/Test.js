08521740214d104d12f52e2e76f38cee82faaf5f
Sajid-Hasan2007 <sajidhasanapp@gmail.com>
2023-03-25 12:53:28 +0600

    Day-8

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,43 +1,21 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-function Test() {
-  const [text, setText] = useState("");
-  const [select, setSelect] = useState("");
-  const [checkbox, setCheckbox] = useState(false);
-
-  return (
-    <div>
-      <h1>Form</h1>
-      <label>Email</label>
-      <input
-        onChange={(e) => {
-          setText(e.target.value);
-        }}
-      />
-      <br />
-      <select
-        onChange={(e) => {
-          setSelect(e.target.value);
-        }}
-      >
-        <option>Sajid</option>
-        <option>fahim</option>
-        <option>kadir</option>
-      </select>
-      <input
-        type="checkbox"
-        name=""
-        id=""
-        onChange={(e) => {
-          setCheckbox(e.target.checked);
-        }}
-      />
-      <button onClick={() => console.log(text, select, checkbox)}>
-        submit
-      </button>
-      <h3>{text}</h3>
-    </div>
-  );
+export class Test extends Component {
+  constructor() {
+    super();
+    console.log("constructor");
+    this.state = {
+      name: "sajid",
+    };
+  }
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>Hello {this.state.name}</h1>
+      </div>
+    );
+  }
 }
 
 export default Test;
