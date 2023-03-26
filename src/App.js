ea5b770f701e792ec5e28891e20cab3aab834f44
Sajid-Hasan2007 <sajidhasanapp@gmail.com>
2023-03-26 16:01:57 +0600

    Day-14

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React, { Component } from "react";
-import Test from "./components/Test";
-
-export class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      show: false,
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.show ? <Test /> : null}
-        <button onClick={() => this.setState({ show: !this.state.show })}>
-          Click
-        </button>
-      </div>
-    );
-  }
+import { useState } from "react";
+function App() {
+  const [loggedIn, setLoggedIn] = useState(3);
+  // loggedIn=3
+  // 1,2,3
+  return (
+    <div>
+      {loggedIn == 1 ? (
+        <h1>Welcome User 1</h1>
+      ) : loggedIn == 2 ? (
+        <h1>Welcome User2</h1>
+      ) : (
+        <h1>Welcome User3</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
